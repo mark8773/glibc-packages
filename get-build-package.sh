edbceb15487f570fc21982de2151a3981d54c9eb
mark8773 <158565946+mark8773@users.noreply.github.com>
2024-05-13 22:06:45 +0300

    123

--- get-build-package.sh
+++ get-build-package.sh
@@ -4,7 +4,7 @@
 
 BRANCH="master"
 
-git clone --depth 1 -b ${BRANCH} --single-branch https://github.com/termux/termux-packages.git
+git clone --depth 1 -b ${BRANCH} --single-branch https://github.com/mark8773/termux-packages.git
 
 for i in build-package.sh clean.sh packages x11-packages scripts; do
 	cp -r ./termux-packages/${i} ./
